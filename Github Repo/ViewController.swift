b94040f1552b98782e129ac21d680e813bb5bf90
Suprianto D <ersaka96@gmail.com>
2019-07-11 20:39:45 +0800

    Initial Commit

---
+++ Github Repo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github Repo
+//
+//  Created by Suprianto Djamalu on 11/07/19.
+//  Copyright © 2019 Suprianto Djamalu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
